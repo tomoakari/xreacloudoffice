288ecfc8a3d34b33878bf2fc7f0584c74d6c59e7
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2021-11-09 00:06:55 +0900

    option.css　追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container bg_login">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,18 +69,5 @@
             </div>
         </div>
     </div>
-
-<style>
-    .container {
-                /* background-color: #fff; */
-                background-image: url(https://aice.cloud/img/conference2.jpg);
-                background-size: cover;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-</style>
 </div>
 @endsection
