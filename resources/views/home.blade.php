2208730a9c06092998f0aa2a117090c5e8bd4db3
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2021-11-08 21:45:10 +0900

    ダッシュボードをちょっと修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">ダッシュボード</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -15,9 +15,9 @@
                     @endif
 
                     <ul>
-                        <li><a href="/mypage">mypage</a></li>
-                        <li><a href="/conference">conference</a></li>
-                        <li><a href="/company">company</a></li>
+                        <li><a href="/mypage">マイページ</a></li>
+                        <li><a href="/conference">会議管理</a></li>
+                        <li><a href="/company">会社管理</a></li>
                     </ul>
                 </div>
             </div>
