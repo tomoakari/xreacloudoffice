5c34cdd871eac2137bf9c456604bdb29d6635d3d
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2021-12-16 17:43:57 +0900

    test

--- public/js/app.js
+++ public/js/app.js
@@ -49794,7 +49794,25 @@ Vue.component('example-component', __webpack_require__(/*! ./components/ExampleC
  */
 
 var app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: function () {
+        return {
+            message: "hello",
+        }
+    },
+    methods: {
+        test() {
+            alert("tyrt");
+            /*
+            Swal.fire({
+                title: 'Error!',
+                text: 'Do you want to continue',
+                icon: 'error',
+                confirmButtonText: 'Cool'
+            })
+            */
+        }
+    }
 });
 
 /***/ }),
--- resources/js/app.js
+++ resources/js/app.js
@@ -29,23 +29,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
-    data: function () {
-        return {
-            message: "hello",
-        }
 
-    },
-    methods: {
-        test() {
-            alert("tyrt");
-            /*
-            Swal.fire({
-                title: 'Error!',
-                text: 'Do you want to continue',
-                icon: 'error',
-                confirmButtonText: 'Cool'
-            })
-            */
-        }
-    }
 });
