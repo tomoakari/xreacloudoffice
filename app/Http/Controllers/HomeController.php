676c2678b2e797927914e7d982b1c86e3862b0eb
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2021-12-16 15:37:35 +0900

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -93,8 +93,8 @@ class HomeController extends Controller
         $innerConfs = Conference::all();
         */
 
-            $outerConfs = Conference::where('innerflg', 0);
-            $innerConfs = Conference::where('innerflg', 1);
+            $outerConfs = Conference::where('innerflg', 0)->get();
+            $innerConfs = Conference::where('innerflg', 1)->get();
         
         return view('home', ['outerConfs' => $outerConfs, 'innerConfs' => $innerConfs]);
         // return view('home');
