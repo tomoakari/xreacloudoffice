1c64661fc643d4401f394ff659f4f20388514dea
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2021-12-22 15:51:36 +0900

    test

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -6,6 +6,7 @@
           <div class="card-header">Example Component</div>
 
           <div class="card-body">I'm an example component.</div>
+          ghjkl
         </div>
       </div>
     </div>
