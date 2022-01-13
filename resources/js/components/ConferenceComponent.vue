5eb0e8b0268dd1da1a6fa6889428752aa884d7ff
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2022-01-14 00:15:34 +0900

    test

--- resources/js/components/ConferenceComponent.vue
+++ resources/js/components/ConferenceComponent.vue
@@ -176,8 +176,40 @@ export default {
               <option value="30">30</option>
               <option value="31">31</option>
             </select>日<br>` +
-            `<input id="input_hour" class="swal2-input" placeholder="00">時　
-            <input id="input_minut" class="swal2-input" placeholder="00">分`,
+            `<select id="input_hour">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+              <option value="13">13</option>
+              <option value="14">14</option>
+              <option value="15">15</option>
+              <option value="16">16</option>
+              <option value="17">17</option>
+              <option value="18">18</option>
+              <option value="19">19</option>
+              <option value="20">20</option>
+              <option value="21">21</option>
+              <option value="22">22</option>
+              <option value="23">23</option>
+              <option value="24">24</option>
+            </select>時　` +
+            `<select id="input_date">
+              <option value="00">00</option>
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="30">30</option>
+              <option value="40">40</option>
+              <option value="50">50</option>
+            </select>分`,
           confirmButtonText: "次へ",
           focusConfirm: false,
           preConfirm: () => {
