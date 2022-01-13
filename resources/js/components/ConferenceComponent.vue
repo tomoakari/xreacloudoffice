a9381c0d13d133ee6e7a1b02b88ae44a9472d72a
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2022-01-13 23:17:02 +0900

    test

--- resources/js/components/ConferenceComponent.vue
+++ resources/js/components/ConferenceComponent.vue
@@ -119,10 +119,59 @@ export default {
         Swal.fire({
           title: "会議情報の登録",
           html:
-            `<select>
-            
-            </select> ` +
-            `<input id="input_schedule" class="swal2-input" placeholder="開催日（2021-04-20 09:30:00）">`,
+            `<select name="input_year">
+              <option value="2021">2021</option>
+              <option value="2022">2022</option>
+              <option value="2022">2023</option>
+            </select>年` +
+            `<select name="input_month">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+            </select>月` +
+            `<select name="input_date">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+              <option value="13">13</option>
+              <option value="14">14</option>
+              <option value="15">15</option>
+              <option value="16">16</option>
+              <option value="17">17</option>
+              <option value="18">18</option>
+              <option value="19">19</option>
+              <option value="20">20</option>
+              <option value="21">21</option>
+              <option value="22">22</option>
+              <option value="23">23</option>
+              <option value="24">24</option>
+              <option value="25">25</option>
+              <option value="26">26</option>
+              <option value="27">27</option>
+              <option value="28">28</option>
+              <option value="29">29</option>
+              <option value="30">30</option>
+              <option value="31">31</option>
+            </select>日`,
+          //`<input id="input_schedule" class="swal2-input" placeholder="開催日（2021-04-20 09:30:00）">`,
           confirmButtonText: "作成",
           focusConfirm: false,
           preConfirm: () => {
