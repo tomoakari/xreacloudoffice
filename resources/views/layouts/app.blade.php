fee7f17a2ccb3455d104d616b4c8fb349179adbb
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2022-01-20 22:09:25 +0900

    mypage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,20 +58,14 @@
                             </li>
                         @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('mypage') }}">{{ マイページ }}</a>
+                            </li>
+                            <!--
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="/mypage" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('mypage') }}">
-                                        マイページ＿
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                                <!--
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
@@ -83,8 +77,8 @@
                                         @csrf
                                     </form>
                                 </div>
--->
                             </li>
+                            -->
                         @endguest
                     </ul>
                 </div>
