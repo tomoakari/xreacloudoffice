c24f2f67f9a13435885c2d46f14c608030916b37
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2022-02-02 19:11:38 +0900

    カンパニー系モデルとファクトリを追加

---
+++ app/Company.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'plan', 'createuserid', 
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+    ];
+}
---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Company;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Company::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'username' => $faker->username,
+        'secret' => $faker->secret,
+        'password' => $faker->password,
+        'innerflg' => $faker->innerflg,
+        'status' => $faker->status,
+        'schedule' => $faker->schedule,
+    ];
+});
