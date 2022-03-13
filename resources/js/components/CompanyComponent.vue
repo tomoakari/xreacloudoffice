7b89742595e6ce592904cba17ab5b2e957d91290
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2022-03-13 15:16:16 +0900

    会社画面表示制御

--- resources/js/components/CompanyComponent.vue
+++ resources/js/components/CompanyComponent.vue
@@ -95,7 +95,7 @@ export default {
           params: {},
         })
         .then((response) => {
-          if (response.result) {
+          if (response.data.result) {
             // 会社登録済み
             this.show_mode = "detail";
           } else {
