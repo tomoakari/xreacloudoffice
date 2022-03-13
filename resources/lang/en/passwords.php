55b0db60234727aa102c2be38585a280b1f12b0f
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2022-03-13 22:19:42 +0900

    メール認証のメッセージを修正

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -12,11 +12,17 @@ return [
     | has failed, such as for an invalid token or invalid new password.
     |
     */
-
+    /*
     'reset' => 'Your password has been reset!',
     'sent' => 'We have e-mailed your password reset link!',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that e-mail address.",
+    */
 
+    'reset' => 'パスワードが正常にリセットされました',
+    'sent' => 'パスワードリセットのURLリンクをメール送信しました',
+    'throttled' => '再試行する前に少々お待ちください',
+    'token' => 'パスワードリセットのURLが間違っている、もしくは期限切れのようです',
+    'user' => "このメールアドレスは見つかりませんでした",
 ];
