104157e267b72151a7c48f355407432abf7b37da
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2022-03-13 12:57:21 +0900

    t

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -127,7 +127,7 @@ class HomeController extends Controller
             return [
                 'result' => 'false',
                 'data' => []
-            ]
+            ];
         } 
     }
 
