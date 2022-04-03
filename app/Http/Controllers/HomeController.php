de40cfb8a2949f0290b63a48b968e76c5cc7231c
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2022-04-03 15:57:34 +0900

    bf

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\User;
 use App\Conference;
 use App\Company;
 use App\Department;
