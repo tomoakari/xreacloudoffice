c41347710a91777defdff59b801f83a963ea4e69
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2022-04-06 21:30:37 +0900

    t

--- database/migrations/2022_04_06_121456_add_columns_users_table.php
+++ database/migrations/2022_04_06_121456_add_columns_users_table.php
@@ -6,23 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 class AddColumnsUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('email_verified')->default(0);
+            $table->string('email_verify_token')->nullable();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('email_verified');
+            $table->dropColumn('email_verify_token');
+        });
     }
 }
