9ac4bed93187d2c3ba63cdbe5bcbd065b830b7d4
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2022-04-17 10:14:03 +0900

    bf

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
      * 定数
      */
     // 0:仮登録 1:本登録 2:メール認証済 9:退会済
-    protected $USER_STATUS => [
+    protected $USER_STATUS = [
         'PRE_REGISTER' => '0',
         'REGISTER' => '1',
         'MAIL_AUTHED' => '2',
