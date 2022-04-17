52f67cecd4cff3e5c6694bbffbf35d5b215cadcd
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2022-04-17 09:19:01 +0900

    bk

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,5 +24,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'email_verify_token' => $faker->email_verify_token,
+        'email_verified' => $faker->email_verified,
     ];
 });
