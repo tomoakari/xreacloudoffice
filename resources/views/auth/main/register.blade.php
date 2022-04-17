8922424581a1472e08a7f8f1b33b83ab406215bd
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2022-04-17 15:34:31 +0900

    t

--- resources/views/auth/main/register.blade.php
+++ resources/views/auth/main/register.blade.php
@@ -14,7 +14,7 @@
 
                     @empty($message)
                         <div class="card-body">
-                            <form method="POST" action="{{ route('register.main_check') }}">
+                            <form method="POST" action="{{ route('register.maincheck') }}">
                                 @csrf
                                 <div class="form-group row">
                                     <label for="name" class="col-md-4 col-form-label text-md-right">名前</label>
