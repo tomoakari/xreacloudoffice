ce9902dd973d42ec325d8990bfdd8332947428c1
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2022-04-17 10:45:18 +0900

    bf

--- resources/views/auth/main/register.blade.php
+++ resources/views/auth/main/register.blade.php
@@ -14,7 +14,7 @@
 
                     @empty($message)
                         <div class="card-body">
-                            <form method="POST" action="{{ route('register.pre_check') }}">
+                            <form method="POST" action="{{ route('register.main_check') }}">
                                 @csrf
                                 <div class="form-group row">
                                     <label for="name" class="col-md-4 col-form-label text-md-right">名前</label>
@@ -110,20 +110,20 @@
                                             @endif
                                         </div>
                                     </div>
-                        </div>
+                                </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    確認画面へ
-                                </button>
-                            </div>
-                        </div>
-                        </form>
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-6 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            確認画面へ
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                    </div>
+                    @endempty
                 </div>
-                @endempty
             </div>
         </div>
-    </div>
     </div>
 @endsection
