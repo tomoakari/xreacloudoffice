e7f65456af8f8c673bb08ca06627c4a32a39d1c7
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2022-06-18 22:51:10 +0900

    修正

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -42,7 +42,10 @@
       </div>
 
       <div class="card-body">
-        <table class="meetinglist" v-show="innerConfs.length > 0">
+        <table
+          class="meetinglist"
+          v-show="innerConfs.length > 0 && !isMobileMode"
+        >
           <tr>
             <th>状況</th>
             <th>日時</th>
@@ -101,6 +104,82 @@
             </td>
           </tr>
         </table>
+        <table
+          class="meetinglist"
+          v-show="innerConfs.length > 0 && isMobileMode"
+        >
+          <tr>
+            <th>状況</th>
+            <th>日時</th>
+            <th>会議名</th>
+            <th></th>
+          </tr>
+          <tr v-for="innerConf in innerConfs" v-bind:key="innerConf.id">
+            <td>
+              <span
+                v-show="getStatusViewFlg(innerConf) == -1"
+                style="color: #f08080"
+                >中止</span
+              >
+              <span
+                v-show="getStatusViewFlg(innerConf) == 0"
+                style="color: gray"
+                >予定</span
+              >
+              <span
+                v-show="getStatusViewFlg(innerConf) == 1"
+                style="color: cornflowerblue"
+                >開催</span
+              >
+              <span
+                v-show="getStatusViewFlg(innerConf) == 2"
+                style="color: orange"
+                >開催</span
+              >
+              <span
+                v-show="getStatusViewFlg(innerConf) == 999"
+                style="color: lightgray"
+                >終了</span
+              >
+            </td>
+            <td>{{ getJPcalendar(innerConf.schedule) }}</td>
+            <td>
+              <span
+                v-show="
+                  getStatusViewFlg(innerConf) == -1 ||
+                  getStatusViewFlg(innerConf) == 999
+                "
+              >
+                {{ innerConf.name }}
+              </span>
+              <span
+                v-show="
+                  getStatusViewFlg(innerConf) !== -1 &&
+                  getStatusViewFlg(innerConf) !== 999
+                "
+              >
+                <a
+                  v-bind:href="
+                    'https://conference.aice.cloud/?secret=' +
+                    innerConf.secret +
+                    '&user_name=' +
+                    user_name
+                  "
+                  target="_blank"
+                >
+                  {{ innerConf.name }}
+                </a>
+              </span>
+            </td>
+            <td>
+              <span
+                class="infobutton"
+                @click="showDetailWindow(1, innerConf.id)"
+                ><i class="far fa-edit"></i
+              ></span>
+            </td>
+          </tr>
+        </table>
         <p v-show="innerConfs.length == 0">
           本日、予定されている会議はありません。
         </p>
@@ -115,7 +194,10 @@
       </div>
 
       <div class="card-body">
-        <table class="meetinglist" v-show="outerConfs.length > 0">
+        <table
+          class="meetinglist"
+          v-show="outerConfs.length > 0 && !isMobileMode"
+        >
           <tr>
             <th>状況</th>
             <th>日時</th>
@@ -174,6 +256,80 @@
             </td>
           </tr>
         </table>
+        <table
+          class="meetinglist"
+          v-show="outerConfs.length > 0 && isMobileMode"
+        >
+          <tr>
+            <th>状況</th>
+            <th>日時</th>
+            <th>会議名</th>
+            <th></th>
+          </tr>
+          <tr v-for="outerConf in outerConfs" v-bind:key="outerConf.id">
+            <td>
+              <span
+                v-show="getStatusViewFlg(outerConf) == -1"
+                style="color: #f08080"
+                >中止</span
+              >
+              <span
+                v-show="getStatusViewFlg(outerConf) == 0"
+                style="color: gray"
+                >予定</span
+              >
+              <span
+                v-show="getStatusViewFlg(outerConf) == 1"
+                style="color: cornflowerblue"
+                >開催</span
+              >
+              <span
+                v-show="getStatusViewFlg(outerConf) == 2"
+                style="color: orange"
+                >開催</span
+              >
+              <span
+                v-show="getStatusViewFlg(outerConf) == 999"
+                style="color: lightgray"
+                >終了</span
+              >
+            </td>
+            <td>{{ getJPcalendar(outerConf.schedule) }}</td>
+            <td>
+              <span
+                v-show="
+                  getStatusViewFlg(outerConf) == -1 ||
+                  getStatusViewFlg(outerConf) == 999
+                "
+                >{{ outerConf.name }}
+              </span>
+              <span
+                v-show="
+                  getStatusViewFlg(outerConf) !== -1 &&
+                  getStatusViewFlg(outerConf) !== 999
+                "
+              >
+                <a
+                  v-bind:href="
+                    'https://conference.aice.cloud/?secret=' +
+                    outerConf.secret +
+                    '&user_name=' +
+                    user_name
+                  "
+                  target="_blank"
+                  >{{ outerConf.name }}</a
+                >
+              </span>
+            </td>
+            <td>
+              <span
+                class="infobutton"
+                @click="showDetailWindow(0, outerConf.id)"
+                ><i class="far fa-edit"></i
+              ></span>
+            </td>
+          </tr>
+        </table>
         <p v-show="outerConfs.length == 0">
           本日、予定されている会議はありません。
         </p>
@@ -188,6 +344,7 @@
 export default {
   data: function () {
     return {
+      isMobileMode: false,
       user_name: "temp_user_name",
       outerConfs: [],
       innerConfs: [],
@@ -200,8 +357,22 @@ export default {
     this.getTodayInnerConfs();
     this.getTodayOuterConfs();
     this.user_name = document.getElementById("login_user_name").value;
+    this.isMobileMode = this.isMobile();
   },
   methods: {
+    isMobile: function () {
+      var userAgent = window.navigator.userAgent.toLowerCase();
+      if (
+        userAgent.indexOf("iphone") != -1 ||
+        userAgent.indexOf("ipad") != -1 ||
+        userAgent.indexOf("android") != -1 ||
+        userAgent.indexOf("mobile") != -1
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
     showDetail: function (id) {
       Swal.fire({
         title: "id: " + id,
@@ -241,18 +412,20 @@ export default {
         .finally();
     },
     getJPcalendar(timestamp) {
+      var youbi = ["日", "月", "火", "水", "木", "金", "土"];
       var dt = new Date(timestamp);
-      console.log("■■■getJPcalendar");
-      console.log("timestamp: " + timestamp);
-      console.log("dt: " + dt);
-
       // var yy = dt.getFullYear();
-      var MM = dt.getMonth() + 1;
-      var dd = dt.getDate();
-      var hh = ("00" + dt.getHours()).slice(-2);
-      var mm = ("00" + dt.getMinutes()).slice(-2);
-      // return /*yy + "年" +*/ MM + "月" + dd + "日" + hh + "時" + mm + "分";
-      return MM + "/" + dd + " " + hh + ":" + mm;
+      var MM = (" " + (dt.getMonth() + 1)).slice(-2);
+      var dd = (" " + dt.getDate()).slice(-2);
+      var hh = (" " + dt.getHours()).slice(-2);
+      var mm = ("0" + dt.getMinutes()).slice(-2);
+      var yb = youbi[dt.getDay()];
+
+      if (this.isMobileMode) {
+        return MM + "/" + dd + " " + hh + ":" + mm;
+      } else {
+        return MM + "月" + dd + "日(" + yb + ") " + hh + "時" + mm + "分";
+      }
     },
     getStatusViewFlg(conf) {
       var st = new Date(conf.schedule);
